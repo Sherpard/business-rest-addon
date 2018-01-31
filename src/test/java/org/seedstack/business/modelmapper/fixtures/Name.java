2b3a0da56b3f81008a049fa5ca4af423e6fba49f
Sherpard2 <xianbtrigo@gmail.com>
2018-01-31 21:58:46 +0100

    clonned from modelmapper addon

---
+++ src/test/java/org/seedstack/business/modelmapper/fixtures/Name.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2013-2017, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.seedstack.business.modelmapper.fixtures;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
