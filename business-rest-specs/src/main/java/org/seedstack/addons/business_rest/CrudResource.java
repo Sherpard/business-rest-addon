d5f9bdcc29db8e38002b75794c9469eb15e5eb9a
Sherpard <xianbtrigo@gmail.com>
2018-02-01 00:54:32 +0100

    Updated Readme && added CrudResource Annotation

---
+++ business-rest-specs/src/main/java/org/seedstack/addons/business_rest/CrudResource.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2013-2017, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.addons.business_rest;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+public @interface CrudResource {
+	String value() default "";
+
+	boolean create() default true;
+
+	boolean read() default true;
+
+	boolean update() default true;
+
+	boolean delete() default true;
+}
